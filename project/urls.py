686284622f38edad793ac946fa8bf6cca22b4a3e
LinuxEater <moisessouzasantos001@gmail.com>
2025-12-26 05:44:32 -0300

    feat: products and products_detail pages and backend logic

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home_view, supplier_list_view, vendor_list_view
+from .views import home_view, supplier_list_view, vendor_list_view, product_list_view, product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('suppliers/', supplier_list_view, name='supplier_list'),
     path('vendors/', vendor_list_view, name='vendor_list'),
+    path('products/', product_list_view, name='product_list'),
+    path('products/<uuid:pk>/', product_detail_view, name='product_detail'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Supplier, Vendor # Import the models
+from .models import Supplier, Vendor, Product # Import the models
 
 @login_required
 def home_view(request):
@@ -23,3 +23,21 @@ def vendor_list_view(request):
         'page_title': 'Administração de Vendedores'
     }
     return render(request, 'vendor_list.html', context)
+
+@login_required
+def product_list_view(request):
+    products = Product.objects.all().order_by('-created_at')
+    context = {
+        'products': products,
+        'page_title': 'Lista de Produtos'
+    }
+    return render(request, 'product/products.html', context)
+
+@login_required
+def product_detail_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product,
+        'page_title': product.name
+    }
+    return render(request, 'product/product_detail.html', context)
