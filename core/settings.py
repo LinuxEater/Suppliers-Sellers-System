a8e64a9b831115d572f7d1d4ecd86d49e95e082b
Moisés Souza <moisessouzasantos001@gmail.com>
2025-12-26 05:50:21 -0300

    Update SECRET_KEY to a placeholder value
    Replaced the secret key with a placeholder for security.

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-klj%j9gq9)gvs&uu%amunalt1*pa$8p21__c#@_(+*@d4p#)av'
+SECRET_KEY = '############################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
